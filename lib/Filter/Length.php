a41f09f965295bca5b21d94ddca7526496ed0187
Nicholas Rawlings <git@nicholasorenrawlings.com>
2014-02-09 12:24:02 -0500

    Inline documentation for the Length filter Also adds a check that the maximum length should be greater than the miniumum

--- lib/Filter/Length.php
+++ lib/Filter/Length.php
@@ -4,20 +4,46 @@ use \Exception;
 use \Proper\Filter;
 
 
+/**
+	The Length filter validates the length of strings, arrays, and countable objects.
+	
+	When using multibyte encodings (such as UTF-8), note that this filter counts the number of *characters* in a string, not the number of bytes.
+**/
 class Length
 implements Filter
 {
+	/**
+		The minimum acceptable length.
+		
+		@var integer
+	**/
 	protected $min;
+	
+	
+	/**
+		The maximum acceptable length.
+		
+		@var integer
+	**/
 	protected $max;
 	
 	
-	public function __construct($params)
+	/**
+		Initializes the filter with an object containing one or both of the following keys:
+		- **min**: The minimum acceptable length, as a nonnegative integer.
+		- **max**: The maximum acceptable length, as a nonnegative integer.
+		
+		@param   object $rules  The miniumum and/or maxiumum length.
+		@throws  Exception      When either the minimum or maxiumum length is not a nonnegative integer.
+		@throws  Exception      When the maximum length is less than the minimum.
+	**/
+	public function __construct($rules)
 	{
-		if (isset($params->min))
+		if (isset($rules->min))
 		{
-			if (is_int($params->min) && $params->min >= 0)
+			if (is_int($rules->min) && $rules->min >= 0)
 			{
-				$this->min = $params->min;
+				$this->min = $rules->min;
 			}
 			else
 			{
@@ -25,11 +51,18 @@ implements Filter
 			}
 		}
 		
-		if (isset($params->max))
+		if (isset($rules->max))
 		{
-			if (is_int($params->max) && $params->max >= 0)
+			if (is_int($rules->max) && $rules->max >= 0)
 			{
-				$this->max = $params->max;
+				if (!is_null($this->min) && $rules->max >= $this->min)
+				{
+					$this->max = $rules->max;
+				}
+				else
+				{
+					throw new Exception('Maximum length must be greater than minimum length');
+				}
 			}
 			else
 			{
@@ -39,6 +72,13 @@ implements Filter
 	}
 	
 	
+	/**
+		Validates the length of the given string, array, or countable object.
+		
+		@param   string|array|Countable  The string, array, or object  to be validated.
+		@return  string|array|Countable  The given value, if it satisfies the filter's requirements.
+		@throws  Exception               When the given value is shorter than the miminum length or longer than the maximum length.
+	**/
 	public function apply($value)
 	{
 		if (is_array($value) || (is_object($value) && $value instanceof \Countable))
