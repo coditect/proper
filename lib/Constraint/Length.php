cd1b52c310c3ce1059719e0cc627ac4d41449bc2
Nicholas Rawlings <git@nicholasorenrawlings.com>
2014-02-10 22:08:50 -0500

    Convert Length filter to Length constraint and make it inherit from Range

---
+++ lib/Constraint/Length.php
@@ -0,0 +1,30 @@
+<?php namespace Proper\Constraint;
+
+
+/**
+	The Length constraint validates the length of strings, arrays, and countable objects.
+	
+	When using multibyte encodings (such as UTF-8), note that this constraint counts the number of *characters* in a string, not the number of bytes.
+**/
+class Length
+extends Range
+{
+	/**
+		Validates the length of the given string, array, or countable object.
+		
+		@param   string|array|Countable  The string, array, or object  to be validated.
+		@return  string|array|Countable  The given value, if it satisfies the constraint's requirements.
+		@throws  Exception               When the given value is shorter than the miminum length or longer than the maximum length.
+	**/
+	public function apply($value)
+	{
+		if (is_array($value) || (is_object($value) && $value instanceof \Countable))
+		{
+			return parent::apply(count($value));
+		}
+		else
+		{
+			return parent::apply(mb_strlen((string) $value));
+		}
+	}
+}
